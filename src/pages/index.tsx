412ddaa7725d8a8eb14ad00ad6ff64cf013f86e5
dhoniaridho <dhoniaridho@gmail.com>
2022-06-16 23:23:07 +0700

    init: initialize ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,285 +1,63 @@
-import type { NextPage } from "next";
-import {
-  BsGithub,
-  BsLinkedin,
-  BsFacebook,
-  BsArrowUpRight,
-} from "react-icons/bs";
 import getAge from "@/helpers/getMyAge";
+import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <header className="fixed inset-0 z-50 w-full top-0 h-20">
+    <div className="bg-slate-900">
+      <header className="sticky inset-0 z-50 w-full top-0 h-20">
         <div className="container mx-auto h-full flex items-center justify-between px-5">
-          <h1 className="text-3xl font-extrabold">D</h1>
-          <nav>
-            <ul className="flex gap-5">
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#about">About</a>
-              </li>
-            </ul>
+          <nav className="flex justify-between w-full">
+            <button className="space-y-3">
+              <span className="block h-1 w-10 bg-white" />
+              <span className="block h-1 w-10 bg-white" />
+            </button>
+            <Link href="/">
+              <a className="text-white">
+                <svg
+                  className="w-16"
+                  viewBox="0 0 105 62"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12.983 61h.526l.298-.434 16.928-24.655.003-.004L45.4 14.824V61h10.38V1.545H42.395l-.297.44L3.172 59.439 2.115 61h10.868Zm45.911-1v1h8.945V38.557h11.544c.294 0 .585-.003.873-.01l11.8 21.927.282.526h10.349l-.81-1.48L89.475 36.84c.454-.188.893-.391 1.316-.61l.005-.003c3.018-1.581 5.292-3.777 6.776-6.59 1.468-2.782 2.188-5.948 2.188-9.473 0-3.524-.72-6.707-2.185-9.523-1.483-2.85-3.765-5.076-6.802-6.677-3.064-1.634-6.917-2.418-11.502-2.418H58.894V60ZM79.16 30.273H67.84V9.717h11.208c3.01 0 5.33.466 7.025 1.323l.007.004c1.71.848 2.914 2.027 3.666 3.534.777 1.558 1.183 3.41 1.183 5.585 0 2.177-.406 3.995-1.177 5.49l-.002.003c-.727 1.425-1.914 2.551-3.627 3.367l-.004.002c-1.682.81-3.982 1.248-6.959 1.248Z" />
+                </svg>
+              </a>
+            </Link>
           </nav>
         </div>
       </header>
-      <main className="mt-20">
-        <section className="relative min-h-[calc(100vh-5rem)] container mx-auto w-full px-5">
-          <div className="py-4 pb-32 sm:pb-0 md:py-10">
-            <div className="w-full rounded-lg bg-sky-100 min-h-[25rem] md:flex px-5 md:px-10 py-16 gap-10 space-y-5">
-              <div className="flex-1">
-                <h3 className="text-4xl md:text-5xl font-extrabold">Ahmad Ridhoni</h3>
-                <h3 className="text-xl md:text-3xl font-bold mt-3">
-                  Design, Development, Ship
-                </h3>
-                <p className="md:text-xl mt-5 font-light">
-                  I&apos;m {getAge("2001/07/18")} year old a full-stack
-                  developer with a passion for building beautiful, performant
-                  user interfaces.
-                </p>
-                <div className="mt-8">
-                  <button
-                    type="button"
-                    className="text-white bg-black font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2"
-                  >
-                    Contact Me
-                  </button>
-                  <button
-                    type="button"
-                    className="text-black bg-transparent hover:bg-black hover:text-white border border-black font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2"
-                  >
-                    Show Projects
-                  </button>
-                </div>
-              </div>
-              <div className="flex-1 h-96">
-                <iframe
-                  className="rounded-lg"
-                  src="https://my.spline.design/macbookprocopy-7c85c3e239a4471dc5558dc850139459/"
-                  frameBorder="0"
-                  width="100%"
-                  height="100%"
-                ></iframe>
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center justify-center rotate-90 mr-5 absolute right-0 bottom-0 md:bottom-10 origin-right">
-            <div className="w-40 border hidden md:block" />
-            <p className="px-5">Scroll Down</p>
-          </div>
-          <div className="flex items-center justify-center gap-5 absolute left-0 bottom-10 text-2xl px-5">
-            <div className="text-lg mr-5 hidden md:block">Checkout My</div>
-            <button className="px-3 py-3 bg-slate-100 rounded-lg">
-              <BsGithub />
-            </button>
-            <button className="px-3 py-3 bg-slate-100 rounded-lg">
-              <BsFacebook />
-            </button>
-            <button className="px-3 py-3 bg-slate-100 rounded-lg">
-              <BsLinkedin />
-            </button>
-          </div>
-        </section>
-        <section className="bg-amber-200 py-20">
-          <div className="container mx-auto w-full px-5">
-            <div className="py-10 md:flex gap-10 space-y-8">
-              <div className="md:w-96 space-y-5">
-                <h3 className="text-5xl font-extrabold uppercase">
-                  Service i Offer <sub className="font-light"> /2</sub>
-                </h3>
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa
-                  nam, recusandae similique facilis unde numquam porro. Nam,
-                  nostrum libero aut aliquid eius unde, aliquam assumenda culpa
-                  rem labore architecto doloribus!
-                </p>
-              </div>
-              <div className="flex-1 grid sm:grid-cols-2 gap-10 flex-wrap">
-                <div className="border-t-4 border-black">
-                  <h5 className="font-semibold mt-2 text-2xl">1/</h5>
-                  <h5 className="font-bold text-2xl md:text-4xl mt-3">
-                    Frontend Development
-                  </h5>
-                  <p className="inline-flex gap-2 items-center text-lg mt-3">
-                    Explore
-                    <BsArrowUpRight />
-                  </p>
-                </div>
-                <div className="flex-1 border-t-4 border-black">
-                  <h5 className="font-semibold mt-2 text-2xl">2/</h5>
-                  <h5 className="font-bold text-2xl md:text-4xl mt-3">
-                    Backend Development
-                  </h5>
-                  <p className="inline-flex gap-2 items-center text-lg mt-3">
-                    Explore
-                    <BsArrowUpRight />
-                  </p>
-                </div>
-                <div className="border-t-4 border-black">
-                  <h5 className="font-semibold mt-2 text-2xl">3/</h5>
-                  <h5 className="font-bold text-2xl md:text-4xl mt-3">
-                    Fullstack Development
-                  </h5>
-                  <p className="inline-flex gap-2 items-center text-lg mt-3">
-                    Explore
-                    <BsArrowUpRight />
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="container mx-auto w-full mt-20 px-5 py-10 md:py-20">
-          <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 gap-10">
-            <div className="space-y-4">
-              <h3 className="text-4xl font-extrabold uppercase">
-                Skill & Experience
-                <sub className="font-light"> /3</sub>
-              </h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Adipisci vel rerum itaque corporis, accusantium eaque repellat
-                nobis aperiam dolor nostrum vitae quae odio consectetur earum
-                placeat perspiciatis aliquid ut dignissimos.
-              </p>
-            </div>
-            <div className="row-span-2 md:border-l md:pl-10">
-              <h3 className="text-3xl font-extrabold uppercase">Experience</h3>
-              <ul className="mt-10">
-                <li className="flex border-t-4 border-black py-5">
-                  <div className="w-40 font-semibold">2019-2020</div>
-                  <div className="flex-1">
-                    <h3 className="text-xl font-bold">Graphic Designer</h3>
-                    <h6>Explora Digital Print</h6>
-                  </div>
-                </li>
-                <li className="flex border-t-4 border-black py-5">
-                  <div className="w-40 font-semibold">2021-Present</div>
-                  <div className="flex-1">
-                    <h3 className="text-xl font-bold">Frontend Developer</h3>
-                    <h6>Transisi Teknologi Mandiri</h6>
-                  </div>
-                </li>
-                <li className="flex border-t-4 border-black py-5">
-                  <div className="w-40 font-semibold">2021-Present</div>
-                  <div className="flex-1">
-                    <h3 className="text-xl font-bold">
-                      Freelance Frontend Developer
-                    </h3>
-                    <h6>Momofin</h6>
-                  </div>
-                </li>
-              </ul>
-            </div>
-            <div className="space-y-4 max-w-full">
-              <h3 className="text-3xl font-extrabold uppercase">Skill</h3>
-              <p className="font-bold">Frontend</p>
-              <ul className="flex gap-5 flex-wrap">
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>Tailwind CSS</p>
-                </li>
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>Emotion CSS</p>
-                </li>
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>Vue JS</p>
-                </li>
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>React JS</p>
-                </li>
-              </ul>
-              <p className="font-bold">Backend</p>
-              <ul className="flex gap-5 flex-wrap">
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>Express JS</p>
-                </li>
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>Nest JS</p>
-                </li>
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>Laravel</p>
-                </li>
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>Mongo DB</p>
-                </li>
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>My SQL</p>
-                </li>
-              </ul>
-              <p className="font-bold">Programming Language</p>
-              <ul className="flex gap-5 flex-wrap">
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>Javascript</p>
-                </li>
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>Typescirpt</p>
-                </li>
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>PHP</p>
-                </li>
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>Dart</p>
-                </li>
-                <li>
-                  <div className="w-20 h-20 bg-green-500 rounded-lg shadow-lg shadow-green-200 mb-4"></div>
-                  <p>GO (incoming)</p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </section>
-        <section>
-          <div className="container mx-auto py-10 md:py-20 px-5">
-            <h3 className="text-center text-4xl font-extrabold uppercase">
-              Latest Project <sub className="font-light"> /4</sub>
-            </h3>
-            <ul className="grid md:grid-cols-3 gap-5 mt-20">
-              <li className="bg-yellow-300 w-full aspect-[3/4] rounded-lg"></li>
-              <li className="bg-yellow-300 w-full aspect-[3/4] rounded-lg"></li>
-              <li className="bg-yellow-300 w-full aspect-[3/4] rounded-lg"></li>
+      <main>
+        <section className="h-[calc(100vh-5rem)] text-white relative container mx-auto">
+          <div className="absolute bottom-44">
+            <h1 className="text-5xl font-extrabold text-white">
+              Ahmad Ridhoni
+            </h1>
+            <p className="md:text-xl mt-5 font-light">
+              I&apos;m {getAge("2001/07/18")} year old a full-stack developer
+              with a passion for building beautiful, performant user interfaces.
+            </p>
+            <ul className="mt-10 inline-flex gap-3">
+              <li>
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                >
+                  Contact Me
+                </button>
+              </li>
+              <li>
+                <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white">
+                  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    Show Project
+                  </span>
+                </button>
+              </li>
             </ul>
           </div>
-        </section>
-        <section className="bg-amber-200 px-5">
-          <div className="container mx-auto md:flex py-20 h-96 space-y-10">
-            <div className="flex-1">
-              <h3 className="text-3xl font-extrabold uppercase">
-                Ready to get started?
-              </h3>
-              <p className="text-xl font-light mt-8">
-                I&apos;m currently looking for new opportunities. If you&apos;re
-                interested in working with me, please get in touch.
-              </p>
-            </div>
-            <div className="flex-1 flex justify-center items-center">
-              <button
-                type="button"
-                className="text-white bg-black font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2 inline-flex gap-3 items-center"
-              >
-                Get Started <BsArrowUpRight />
-              </button>
-            </div>
-          </div>
-          <footer>
-            <div className="container mx-auto flex h-20 items-center md:py-20 font-bold md:text-left text-center">
-              &copy; dhoniaridho 2022- All Rights Reserved
-            </div>
-          </footer>
+          <span className="absolute right-0 bottom-10 text-slate-400">
+            Scroll down
+          </span>
         </section>
       </main>
     </div>
