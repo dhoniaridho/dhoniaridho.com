6220a79777767c5779e073e1d2ba6a5d938134f8
dhoniaridho <dhoniaridho@gmail.com>
2022-06-19 10:18:08 +0700

    ✨ feat(Projects): Create Projects Page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,21 @@
 import "@/assets/css/globals.css";
 import type { AppProps } from "next/app";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, domAnimation, LazyMotion } from "framer-motion";
 import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <AnimatePresence
-      initial={false}
-      presenceAffectsLayout={false}
-      onExitComplete={() => window.scrollTo(0, 0)}
-    >
-      <Component {...pageProps} key={router.route} />
-    </AnimatePresence>
+    <LazyMotion features={domAnimation}>
+      <AnimatePresence
+        initial={false}
+        presenceAffectsLayout={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
+    </LazyMotion>
   );
 }
 
