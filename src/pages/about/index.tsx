2a83a5f93c2da18786322f0c340119bdb7e1b99e
dhoniaridho <dhoniaridho@gmail.com>
2022-06-20 13:26:02 +0700

    ✨ feat(About): New Page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,11 @@
+import MainLayout from "@/layouts/main.layout";
+
+const About = () => {
+  return (
+    <MainLayout>
+      <div className="h-screen bg-slate-900"></div>
+    </MainLayout>
+  );
+};
+
+export default About;
