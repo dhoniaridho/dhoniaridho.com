c389930ea2ef45d1e575a3fa7e36bd81d26c069a
dhoniaridho <dhoniaridho@gmail.com>
2022-07-07 22:40:17 +0700

    init v2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,92 +4,50 @@ import Link from "next/link";
 import { BsArrowUpRight, BsArrowDown } from "react-icons/bs";
 import Me from "@/assets/images/me.jpg";
 import MainLayout from "@/layouts/main.layout";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-900 max-w-full overflow-x-hidden">
-      <MainLayout className="bg-slate-900">
-        <section className="h-[calc(100vh-5rem)] text-white bg-slate-900 relative">
-          <div className="container mx-auto w-full h-full px-5 relative z-20">
-            <div className="absolute bottom-44">
-              <h1 className="text-3xl md:text-5xl font-extrabold text-white uppercase">
-                Ahmad Ridhoni
-              </h1>
-              <p className="md:text-xl mt-5 font-light">
-                I&apos;m {getAge("2001/07/18")} year old a full-stack developer with a passion for
-                building beautiful, performant user interfaces.
-              </p>
-              <ul className="mt-10 inline-flex gap-3">
-                <li className="flex items-center">
-                  <Link href="/contact">
-                    <a className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white">
-                      <span className="relative px-7 py-3 md:py-5 md:px-12 transition-all ease-in duration-75 bg-white dark:bg-gray-900 group-hover:bg-opacity-0">
-                        Contact Me
-                      </span>
-                    </a>
-                  </Link>
-                </li>
-                <li className="flex items-center">
-                  <Link href="/projects">
-                    <a className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white">
-                      <span className="relative px-7 py-3  md:py-5 md:px-12 transition-all ease-in duration-75 bg-white dark:bg-gray-900 group-hover:bg-opacity-0">
-                        Show Project
-                      </span>
-                    </a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="absolute left-1/2 right-1/2 whitespace-nowrap md:left-auto md:right-20 bottom-10 text-slate-400 flex items-center justify-center gap-1">
-              <span>Scroll down </span>
-              <BsArrowDown className="animate-bounce w-5" />
+    <div className=" max-w-full overflow-x-hidden">
+      <MainLayout className="">
+        <section className="container mx-auto h-[calc(100vh-5rem)] relative grid grid-cols-2 gap-20 p-10">
+          <div className="">
+            <div className="relative rounded-lg w-full h-full overflow-hidden">
+              <Image layout="fill" className="object-cover" src={Me.src} alt="Me" />
             </div>
           </div>
-          <div className="w-full h-screen bg-slate-900/90 absolute inset-0 z-[3]"></div>
-          <div
-            className="w-full h-screen bg-cover bg-center absolute inset-0 z-0"
-            style={{ backgroundImage: `url('${Me.src}')` }}
-          />
-          <div className="absolute w-full h-screen"></div>
+          <div className="py-20">
+            <div className="mt-20">
+              <p className="text-2xl mb-4">HI I&apos;AM</p>
+              <h1 className="text-5xl font-bold font-serif">Ahmad Ridhoni</h1>
+              <h4 className="mt-5">
+                {getAge("18-July-2001")} year old a full-stack developer with a passion for building
+                beautiful, performant user interfaces. I create successful responsive websites that
+                are fast, easy to use, and built with best practices. The main area of my expertise
+                is front-end development, HTML, CSS, JS, building small and medium web apps,
+                features, animations, and coding interactive layouts.
+              </h4>
+            </div>
+            <div className="inline-flex gap-5 mt-10">
+              <button className="bg-[#46e891] px-10 py-4">KNOW MORE</button>
+              <button className="border-2 border-[#46e891] px-10 py-4">SHOW PROJECTS</button>
+            </div>
+          </div>
         </section>
-        <section className="bg-[#46e891] text-slate-900 py-20">
+        <section className="bg-slate-100 text-slate-900 py-20">
           <div className="container mx-auto w-full px-5 relative">
-            <div className="py-10 md:flex gap-10 space-y-8">
-              <div className="md:w-96 space-y-5">
-                <h3 className="text-5xl font-extrabold uppercase">
-                  Service i Offer <sub className="font-light"> /2</sub>
-                </h3>
-                <p>
-                  I’m a Full-Stack Developer located in Yogyakarta. I have a serious passion for UI
-                  effects, animations and creating intuitive, dynamic user experiences.
-                </p>
+            <h3 className="text-5xl font-extrabold uppercase text-center mb-20">
+              Service i Offer <sub className="font-light"> /2</sub>
+            </h3>
+            <div className="flex gap-10">
+              <div className="flex w-1/2">
+                <Card stack="FE" />
+                <Card stack="BE" />
+                <Card stack="FS" />
               </div>
-              <div className="flex-1 grid sm:grid-cols-2 gap-10 flex-wrap">
-                <div className="border-t-4 border-black">
-                  <h5 className="font-semibold mt-2 text-2xl">1/</h5>
-                  <h5 className="font-bold text-2xl md:text-4xl mt-3">Frontend Development</h5>
-                  <a role="button" className="inline-flex gap-2 items-center text-lg mt-3 group">
-                    Explore
-                    <BsArrowUpRight className="group-hover:rotate-45 transition-transform group-hover:focus:translate-x-2" />
-                  </a>
-                </div>
-                <div className="flex-1 border-t-4 border-black">
-                  <h5 className="font-semibold mt-2 text-2xl">2/</h5>
-                  <h5 className="font-bold text-2xl md:text-4xl mt-3">Backend Development</h5>
-                  <a role="button" className="inline-flex gap-2 items-center text-lg mt-3 group">
-                    Explore
-                    <BsArrowUpRight className="group-hover:rotate-45 transition-transform group-hover:focus:translate-x-2" />
-                  </a>
-                </div>
-                <div className="border-t-4 border-black">
-                  <h5 className="font-semibold mt-2 text-2xl">3/</h5>
-                  <h5 className="font-bold text-2xl md:text-4xl mt-3">Fullstack Development</h5>
-                  <a role="button" className="inline-flex gap-2 items-center text-lg mt-3 group">
-                    Explore
-                    <BsArrowUpRight className="group-hover:rotate-45 transition-transform group-hover:focus:translate-x-2" />
-                  </a>
-                </div>
-              </div>
+              <div></div>
             </div>
           </div>
         </section>
@@ -244,7 +202,7 @@ const Home: NextPage = () => {
           </div>
           <div className="bg-gradient-to-tr from-slate-700/60 to-black/40 w-full h-full absolute z-0 inset-0" />
         </section>
-        <section className="bg-slate-900 text-slate-100 px-5">
+        <section className=" text-slate-100 px-5">
           <div className="container mx-auto md:flex py-20 h-96 space-y-10">
             <div className="flex-1">
               <h3 className="text-3xl font-extrabold uppercase">Ready to get started?</h3>
@@ -268,5 +226,53 @@ const Home: NextPage = () => {
     </div>
   );
 };
+
+function Card(props: any) {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleActive = () => setIsActive(!isActive);
+
+  const variants = {
+    open: { fill: "red", stroke: "red" },
+    closed: { stroke: "black", fill: "none" }
+  };
+
+  const flipBackVariants = {
+    open: { rotateY: "180deg" },
+    closed: { rotateY: "0deg" }
+  };
+
+  return (
+    <motion.button
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 1 }}
+      onClick={toggleActive}
+      animate={isActive ? "open" : "close"}
+      variants={flipBackVariants}>
+      <div className="w-48 h-72 bg-white rounded-lg shadow-lg relative p-5 cursor-pointer">
+        <span className="absolute right-5 text-xl font-bold font-serif">{props.stack}</span>
+        <span className="absolute bottom-5 left-5 text-xl font-bold font-serif">{props.stack}</span>
+        <div className="w-full h-full flex justify-center items-center">
+          <motion.div
+            whileTap={{ scale: 1.5 }}
+            animate={isActive ? "open" : "closed"}
+            variants={variants}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-20 w-20"
+              viewBox="0 0 24 24"
+              strokeWidth={1.3}>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>
+          </motion.div>
+        </div>
+      </div>
+    </motion.button>
+  );
+}
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/layouts/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      serif: ["Merriweather", "ui-serif", "Georgia"]
+    },
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
